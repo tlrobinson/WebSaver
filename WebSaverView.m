2c762a185a531e61d769ee660461d1030d6656e0
Pekka Nikander <pekka.nikander@senseg.com>
2012-05-16 20:43:07 +0300

    Change bundle ID and add Copyright notice

--- WebSaverView.m
+++ WebSaverView.m
@@ -3,7 +3,9 @@
 //  WebSaver
 //
 //  Created by Thomas Robinson on 10/13/09.
+//  Modified by Pekka Nikander in May 2012.
 //  Copyright (c) 2009, 280 North. All rights reserved.
+//  Copyright (c) 2012, Senseg.  All rights reserved.
 //
 
 #import "WebSaverView.h"
